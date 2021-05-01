c03a1b24232395f090770bb20419408402da664c
g-vega-cl <g.vega.cl@gmail.com>
2021-05-01 09:44:12 -0500

    Most changes now are frontend and react. Will just copy-paste code until we get to ruby again

--- app/javascript/components/Airline/Airline.tsx
+++ app/javascript/components/Airline/Airline.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./Header";
 
-const Airline = () =>{
-  return(<div>This is the Airlines#show view for the app</div>)
-}
+const Airline = ({ match }) => {
+  const [airline, setAirline] = useState({});
+  const [review, setRreview] = useState({});
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    const url = `/api/v1/airlines/${match.params.slug}`;
+
+    axios
+      .get(url)
+      .then((resp) => setAirline(resp.data))
+      .catch((resp) => console.log("500 ", resp));
+  }, []);
+
+  return (
+    <div className="wrapper">
+      <div className="column">
+        <Header attributes={airline.data.attributes}/>
+        <div className="reviews"></div>
+      </div>
+      <div className="column">
+        <div className="review-form">Here we add review form</div>
+      </div>
+    </div>
+  );
+};
 
 export default Airline;
--- app/javascript/components/Airlines/Airlines.tsx
+++ app/javascript/components/Airlines/Airlines.tsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Airline from "./Airline";
+import styled from 'styled-components';
 
-const Airlines = () =>{
-  return(<div>This is the Airlines#index view for the app</div>)
-}
+const Home = styled.div`
+  text-align: center;
+  min-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+`
+const Header = styled.div`
+  padding: 100px 100px 10px 100px;
+
+  h1 {
+    font-size: 42px;
+  }
+`
+const Subheader = styled.div`
+  font-weight: 300;
+  font-size:26px;  
+`
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+  padding:20px;
+`
+
+const Airlines = () => {
+  const [airlines, setAirlines] = useState([]);
+
+  useEffect(() => {
+    //Get airlines from API. using axios.
+    axios
+      .get("/api/v1/airlines.json")
+      .then((resp) => setAirlines(resp.data.data))
+      .catch((resp) => console.log("catch ", resp));
+  }, [airlines.length]);
+
+  const grid = airlines.map((item) => {
+    return (
+      <Airline
+        key={item.attributes.name}
+        name={item.attributes.name}
+        image_url={item.attributes.image_url}
+        slug={item.attributes.slug}
+        score={item.attributes.avg_score}
+      />
+    );
+  });
+
+  return (
+    <Home>
+      <Header>
+        <h1>OpenFlights</h1>
+        <Subheader>Honest, unbiased airline reviews.</Subheader>
+      </Header>
+      <Grid>
+        {grid}
+      </Grid>
+    </Home>
+  );
+};
 
 export default Airlines;
